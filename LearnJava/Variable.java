d62db082b3c74acdccbc7f9373311c969543f5dd
M Akbar <148222478+Arzstyle@users.noreply.github.com>
2023-10-29 14:26:43 +0700

    Add files via upload

---
+++ LearnJava/Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+    public static void main(String[] args) {
+        
+        String name = "M Akbar Rizky";
+        System.out.println(name);
+
+        int age = 19;
+        System.out.println(age);
+
+        String address = "Indonesia";
+        System.out.println(address);
+
+        var firstname = "M Akbar ";
+        var middlename = "Rizky";
+        var lastname = "Saputra";
+
+        System.out.println(firstname + middlename + lastname);
+
+        
+        
+    }
+}
