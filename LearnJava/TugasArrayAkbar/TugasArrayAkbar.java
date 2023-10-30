e6125b5e7e8b89a39561fee3503b07bda4a22fdf
M Akbar <148222478+Arzstyle@users.noreply.github.com>
2023-10-30 15:36:00 +0700

    Merevisi mempersingkat logic

--- LearnJava/TugasArrayAkbar/TugasArrayAkbar.java
+++ LearnJava/TugasArrayAkbar/TugasArrayAkbar.java
@@ -9,6 +9,7 @@ public class TugasArrayAkbar {
         };
         
         String findGuest = "X";
+        int emptyRoom = 0;
 
         // f = floor
         // r = room
@@ -17,18 +18,12 @@ public class TugasArrayAkbar {
                 if (hotelAmaris[f][r].equals(findGuest)) {
                     System.out.println("Tamu berada di lantai" + (4 - f) + " Kamar " + (r + 1)) ;
                 }
-            }
-        } 
-        
-        //No 2
-        int emptyRoom = 0;
-        for (int f = 0; f < hotelAmaris.length; f++){
-            for (int r = 0; r < hotelAmaris[f].length; r++){
+                // No 2
                 if (hotelAmaris[f][r] == "*") {
                     emptyRoom++;
-                }        
+                }
             }
-        }
+        } 
         System.out.print("Jumlah Kamar yang tersedia adalah " + emptyRoom + " Kamar ");
     }
 }
