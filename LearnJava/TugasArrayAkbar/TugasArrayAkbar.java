3aac4c391871fafdbcaf5c3024fea6026cc5d6e3
M Akbar <148222478+Arzstyle@users.noreply.github.com>
2023-10-30 12:53:16 +0700

    Update TugasArrayAkbar.java

--- LearnJava/TugasArrayAkbar/TugasArrayAkbar.java
+++ LearnJava/TugasArrayAkbar/TugasArrayAkbar.java
@@ -14,7 +14,7 @@ public class TugasArrayAkbar {
         for (int i = 0; i <= hotelAmaris.length; i++){
             for (int j= 0; j <= hotelAmaris[i].length; j++){
                 if (hotelAmaris[i][i].equals(findGuest)) {
-                    System.out.println("Tamu berada pada lantai" + 4 + " Kamar " + 1 ;
+                    System.out.println("Tamu berada pada lantai" + (4-i) + " Kamar " + (1 + j) ;
                 }
 
             }
