9bdc31767a5fe9b053d5d727911370006a6a6636
M Akbar <148222478+Arzstyle@users.noreply.github.com>
2023-11-03 16:54:27 +0700

    menghapus deklarasi dan menambahkan logic

--- LearnJava/TugasArray2DAkbar/TugasArray2D.java
+++ LearnJava/TugasArray2DAkbar/TugasArray2D.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class TugasArray2D {
     public static void main(String[] args) {
         
-        String pathWay[][] = {
+        String jalurMendaki[][] = {
             {"P", "X", "X", "X", "X", "X", "X", "X", "X", " ", " ", " "},
             {" ", "X", " ", " ", " ", "X", "X", "P3", "X", " ", "X", " "},
             {" ", "X", "X", "X", " ", "P4", " ", " ", " ", " ", "X", " "},
@@ -11,23 +11,28 @@ public class TugasArray2D {
             {" ", " ", "X", " ", " ", "X", "P2", "X", "X", "X", "X", " "},
             {"P5", " ", "X", "RK", " ", "X", " ", " ", " ", " ", " ", "P1"},
         };
-        
+
+        //Jalur 1 = LLLLLUUULKLDDLDKUULLLUUU
+        //Jalur 2 = LLLLLUUULLDDDKLUULLLUUU
+        //Jalur 3 = LLLLUUUUK
+        //Jalur 4 = LLLLLUUULLDDLDUULLLUUU
+         
         Scanner input = new Scanner(System.in);
 
-        /*int P = 0; 
-        int P1 = 0; 
-        int P2 = 0; 
-        int P3 = 0; 
-        int P4 = 0; 
-        int X = 0;*/
+        System.out.print("Masukan tenaga awal : ");
+        int energy = input.nextInt();
 
-        char langkah = 'L';
+        System.out.print("Masukan Jalur : ");
+        String jalur = input.next();
+        
+        input.close();
+
         int posisiBaris = 5;
         int posisiKolom = 12;
-        int energi = 0;
 
-
-        
+        for (int i = 0; i < jalurMendaki.length; i++) {
+                char langkah  = jalurMendaki[i].charAt(i);
+                energy--;
         if (langkah == 'L'){
             posisiBaris = posisiBaris - 1; 
         } else if (langkah == 'R') {
@@ -38,11 +43,21 @@ public class TugasArray2D {
             posisiKolom = posisiKolom + 1;
         } else if (langkah == 'R'){
             energi += 10;
-        } else {
-            System.out.println("Perintah tidak dikenali");
+        } String newPos = jalurMendaki[posisiBaris][posisiKolom];
+            if (newPos.equals("P2")  ||
+                newPos.equals("P3")  ||
+                newPos.equals("P4")  ||
+                newPos.equals("P5")  ||
+                newPos.equals("TC")  ||
+                newPos.equals("RK")) {
+                energy += 10;
+            }else {
+            System.out.println("Mohon maaf, istirahat hanya diperbolehkan di Pos yang tersedia");
+            }
+        }  
+        if (energy <= 0) {
+            System.out.println("Jalur anda benar, tapi tenaga anda tidak akan kuat, coba jalur lain atau sempatkan istirahat terlebih dahulu");
+            return;
         }
-        System.out.println("Koordinat Baris: " + posisiBaris);
-        System.out.println("Koordinat Kolom: " + posisiKolom);
-        
     }
 }
