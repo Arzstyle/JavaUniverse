8df270c6a2457f5d4d649f4430afbf820e40e889
M Akbar <148222478+Arzstyle@users.noreply.github.com>
2023-11-06 20:53:09 +0700

    revisi terakhir

--- LearnJava/TugasArray2DAkbar/TugasArray2D.java
+++ LearnJava/TugasArray2DAkbar/TugasArray2D.java
@@ -12,10 +12,17 @@ public class TugasArray2D {
             {"P5", " ", "X", "RK", " ", "X", " ", " ", " ", " ", " ", "P1"},
         };
 
-        //Jalur 1 = LLLLLUUULKLDDLDKUULLLUUU
-        //Jalur 2 = LLLLLUUULLDDDKLUULLLUUU
-        //Jalur 3 = LLLLUUUUK
-        //Jalur 4 = LLLLLUUULLDDLDUULLLUUU
+        /* Mohon maaf pak untuk R pada nomor 5 nya saya ganti dengan I, dikarenakan saya ngeblank pak
+           tidak menemukan , lalu hasil outputnya selalu tidak sesuai dengan yang bapak cantumkan
+           tapi ketika mencoba dirubah ke I hasilnya keluar sesuai output bapak pak
+
+
+            Tenaga 12 ,  Jalur 1 = LLLLLUUULILDDLDIUULLLUUU
+            Tenaga 100 , Jalur 2 = LLLLLUUULLDDDILUULLLUUU
+            Tenaga 15 ,  Jalur 3 = LLLLUUUUI
+            Tenaga 10 ,  Jalur 4 = LLLLLUUULLDDLDUULLLUUU
+         
+        */
          
         Scanner input = new Scanner(System.in);
 
@@ -28,7 +35,7 @@ public class TugasArray2D {
         input.close();
 
         int posisiPlayer = 5;
-        int posisiKolom = 12;
+        int posisiKolom = 11;
 
         for (int i = 0; i < jalur.length(); i++) {
                 char langkah  = jalur.charAt(i);
@@ -41,7 +48,7 @@ public class TugasArray2D {
                 posisiPlayer--;
             } else if (langkah == 'D' && posisiPlayer < jalurMendaki.length -1 ){
                 posisiPlayer++;
-            } else if (langkah == 'R'){
+            } else if (langkah == 'I'){
             String newPos = jalurMendaki[posisiPlayer][posisiKolom];
             if (newPos.equals("P2")  ||
                 newPos.equals("P3")  ||
@@ -50,20 +57,23 @@ public class TugasArray2D {
                 newPos.equals("TC")  ||
                 newPos.equals("RK")) {
                 energy += 10;
-            }else {
+            } else {
             System.out.println("Mohon maaf, istirahat hanya diperbolehkan di Pos yang tersedia");
             return;
             }
-        }  
-        if (energy <= 0) {
-            System.out.println("Jalur anda benar, tapi tenaga anda tidak akan kuat, coba jalur lain atau sempatkan istirahat terlebih dahulu");
-            return;
+            } else {
+                System.out.println("Tolong masukan input rute yang benar seperti L, R, U, D, I.");
+                return;
+            } 
+            if (energy <= 0) {
+                System.out.println("Jalur anda benar, tapi tenaga anda tidak akan kuat, coba jalur lain atau sempatkan istirahat terlebih dahulu");
+                return;
             } else if (jalurMendaki[posisiPlayer][posisiKolom].equals("X")) {
-            System.out.println("Jalur anda salah, anda masuk ke jurang/blank 45");
-            return;
+                System.out.println("Jalur anda salah, anda masuk ke jurang/blank 45");
+                return;
             } else if (jalurMendaki[posisiPlayer][posisiKolom].equals("P")) {
-            System.out.println("Selamat Pendakian anda berhasil mencapai Puncak Mahameru, sisa tenaga anda " + energy);
-            return;
+                System.out.println("Selamat Pendakian anda berhasil mencapai Puncak Mahameru, sisa tenaga anda " + energy);
+                return;
             }
         }  
     }
